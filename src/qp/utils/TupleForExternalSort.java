326872fe76b22a224cb7988a82ef3fac02f8d461
ChenJiehan318 <e0323526@u.nus.edu>
2021-03-17 22:10:49 +0800

    push untracked changes

--- src/qp/utils/TupleForExternalSort.java
+++ src/qp/utils/TupleForExternalSort.java
@@ -1,4 +1,14 @@
 package qp.utils;
 
 public class TupleForExternalSort {
+
+    public final Tuple tuple;
+    public final int sortedRunNum;// the sorted run number
+    public final int tupleNum;// the tuple number in its sorted run.
+
+    public TupleForExternalSort(Tuple tuple, int sortedRunNum, int tupleNum) {
+        this.tuple = tuple;
+        this.sortedRunNum = sortedRunNum;
+        this.tupleNum = tupleNum;
+    }
 }
